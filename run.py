ef7c08baf07ee0bde2e53f3640c05ea9acf36fff
CMQ1996 <cormacmcquillanfreelance@gmail.com>
2025-07-07 20:53:59 +0100

    added grid, ships and defined turns for battleships game

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+GRID_SIZE = 5
+NUM_SHIPS = 3
+MAX_TURNS = 10
+
+def print_board(board):
+    print("  " + " ".join(str(i) for i in range(GRID_SIZE)))
+    for idx, row in enumerate(board):
